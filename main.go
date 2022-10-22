f78ed9f7749f0f7af71075f8a0fc37abb99f02ef
Darren Liew <ldarren@gmail.com>
2022-10-23 03:14:01 +0800

    used pubsub for sse

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 	"net/http"
+	"encoding/json"
 
 	"github.com/uptrace/bunrouter"
 	"github.com/uptrace/bunrouter/extra/reqlog"
@@ -16,6 +17,7 @@ import (
 )
 
 var broker = sse.NewBroker()
+var ps = pubsub.New(0)
 
 func debugHandler(w http.ResponseWriter, req bunrouter.Request) error {
 	log.Println("Receiving event")
@@ -28,6 +30,10 @@ func debugHandler(w http.ResponseWriter, req bunrouter.Request) error {
 	})
 }
 
+type Message struct {
+	Text string    `json:"text"`
+}
+
 func server(addr string) {
 	router := bunrouter.New(
 		bunrouter.Use(reqlog.NewMiddleware()),
@@ -38,57 +44,66 @@ func server(addr string) {
 		return tmpl.RenderHTML(w, "sse", ably)
 	})
 
-	router.GET("/sse", func(w http.ResponseWriter, req bunrouter.Request) error {
-
-		// Make sure that the writer supports flushing.
-		flusher, ok := w.(http.Flusher)
-
-		if !ok {
-			http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
-			return nil
-		}
-
-		w.Header().Set("Content-Type", "text/event-stream")
-		w.Header().Set("Cache-Control", "no-cache")
-		w.Header().Set("Connection", "keep-alive")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-
-		// Each connection registers its own message channel with the Broker's connections registry
-		messageChan := make(chan []byte)
-
-		// Signal the broker that we have a new connection
-		broker.NewClients <- messageChan
-
-		// Remove this client from the map of connected clients
-		// when this handler exits.
-		defer func() {
-			broker.ClosingClients <- messageChan
-		}()
-
-		// Listen to connection close and un-register messageChan
-		// notify := w.(http.CloseNotifier).CloseNotify()
-		notify := req.Context().Done()
-
-		go func() {
-			<-notify
-			broker.ClosingClients <- messageChan
-		}()
-
-		for {
-
-			// Write to the ResponseWriter
-			// Server Sent Events compatible
-			fmt.Fprintf(w, "data: %s\n\n", <-messageChan)
-
-			// Flush the data immediatly instead of buffering it for later.
-			flusher.Flush()
-		}
-	})
-
 	router.WithGroup("/api", func(g *bunrouter.Group) {
-		g.GET("/users/:id", debugHandler)
-		g.GET("/users/current", debugHandler)
-		g.GET("/users/*path", debugHandler)
+		g.GET("/users/:id/*path", debugHandler)
+
+		g.POST("/topics/:topic", func(w http.ResponseWriter, req bunrouter.Request) error {
+			topic := req.Params().ByName("topic")
+			var body Message
+			if err := json.NewDecoder(req.Body).Decode(&body); err != nil {
+				return err
+			}
+			ps.Pub(body.Text, topic)
+
+			return bunrouter.JSON(w, bunrouter.H{
+				"code":  req.Route(),
+				"params": req.Params().Map(),
+			})
+		})
+
+		g.GET("/topics/:topic", func(w http.ResponseWriter, req bunrouter.Request) error {
+
+			// Make sure that the writer supports flushing.
+			flusher, ok := w.(http.Flusher)
+
+			if !ok {
+				http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
+				return nil
+			}
+
+			w.Header().Set("Content-Type", "text/event-stream")
+			w.Header().Set("Cache-Control", "no-cache")
+			w.Header().Set("Connection", "keep-alive")
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+
+			topic := req.Params().ByName("topic")
+			ch := ps.Sub(topic)
+
+			// Remove this client from the map of connected clients
+			// when this handler exits.
+			defer func() {
+				go ps.Unsub(ch, topic)
+			}()
+
+			// Listen to connection close and un-register messageChan
+			// notify := w.(http.CloseNotifier).CloseNotify()
+			notify := req.Context().Done()
+
+			go func() {
+				<-notify
+				go ps.Unsub(ch, topic)
+			}()
+
+			for {
+
+				// Write to the ResponseWriter
+				// Server Sent Events compatible
+				fmt.Fprintf(w, "data: %s\n\n", <-ch)
+
+				// Flush the data immediatly instead of buffering it for later.
+				flusher.Flush()
+			}
+		})
 	})
 
 	log.Printf("listening on %v", addr)
@@ -128,6 +143,5 @@ func testPubsub() {
 func main() {
 	cfg.LoadJSON()
 	tmpl.Load(cfg.GetStr([]string{"Path", "Asset"}))
-	testPubsub()
 	server(cfg.GetStr([]string{"Server", "Addr"}))
 }
